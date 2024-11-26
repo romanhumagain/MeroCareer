dac4f169774dfe98d566d51db031d85c3f8e8723
romanhumagain <romanhumagain@gmail.com>
2024-11-26 15:44:43 +0545

    configure django for backend

--- backend/src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const {registerUser} = require('../controllers/userControllers')
-
-const router = express.Router();
-
-// api endpoints 
-router.post('/register', registerUser)
-
-module.exports = router;
